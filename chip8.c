620a89cb8ffe20587ca6c74b3020d97fd900bad2
Nicola Carraro <carraron985@gmail.com>
2022-12-25 11:15:42 +0100

    Created global state struct

--- chip8.c
+++ chip8.c
@@ -1,63 +1,72 @@
 #include <Windows.h>
 #include<stdbool.h>
+#include <d3d9.h>
 
 #define clear_struct(obj)(memset(&obj, 0, sizeof(obj)))
 
-boolean global_running = true;
+typedef struct  {
+	boolean running;
+	LPDIRECT3D9 direct3d;   
+	LPDIRECT3DDEVICE9 direct3d_dev;
+} State;
+
+State global_state;
 
 LRESULT CALLBACK WindowProc(HWND window, UINT msg, WPARAM wparam, LPARAM lparam) {
-    switch(msg) {
-    case WM_QUIT: global_running = false;
+	switch (msg) {
+	case WM_QUIT: {global_state.running = false; } break;
 
-    }
+	}
 
-    DefWindowProcA(window, msg, wparam, lparam);
+	DefWindowProcA(window, msg, wparam, lparam);
 }
 
 int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, PSTR cmd_line, int cmd_show) {
 
+	clear_struct(global_state);
 	const char* class_name = "chip8";
 
 	WNDCLASSA wc;
-    clear_struct(wc);
+	clear_struct(wc);
 
 	wc.lpfnWndProc = DefWindowProcA;
 	wc.hInstance = instance;
 	wc.lpszClassName = class_name;
-    if (RegisterClassA(&wc) != 0) {
+	if (RegisterClassA(&wc) != 0) {
 
-        HWND window = CreateWindowExA(
-            0,
-            class_name,
-            "Chip 8",
-            WS_OVERLAPPEDWINDOW,
-            CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
-            0,
-            0,
-            instance,
-            0
-        );
+		HWND window = CreateWindowExA(
+			0,
+			class_name,
+			"Chip 8",
+			WS_OVERLAPPEDWINDOW,
+			CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT, CW_USEDEFAULT,
+			0,
+			0,
+			instance,
+			0
+		);
 
-        if (window != 0) {
-            ShowWindow(window, cmd_show);
-            MSG msg;
-            clear_struct(msg);
+		if (window != 0) {
+			ShowWindow(window, cmd_show);
+			MSG msg;
+			clear_struct(msg);
 
-            while (global_running)
-            {
-                while (PeekMessage(&msg, window, 0, 0, PM_REMOVE)) {
-                    TranslateMessage(&msg);
-                    DispatchMessageA(&msg);
-                }
-            }
-         
-        }
-        else {
-            OutputDebugStringA("Could not open window");
-        }
+			global_state.running = true;
+			while (global_state.running)
+			{
+				while (PeekMessage(&msg, window, 0, 0, PM_REMOVE)) {
+					TranslateMessage(&msg);
+					DispatchMessageA(&msg);
+				}
+			}
 
-    }
-    else {
-        OutputDebugStringA("Could not register window class");
-    }
+		}
+		else {
+			OutputDebugStringA("Could not open window");
+		}
+
+	}
+	else {
+		OutputDebugStringA("Could not register window class");
+	}
 }
