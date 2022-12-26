3a57703a25afc039d6f75828e309e3b866d07ba4
Nicola Carraro <carraron985@gmail.com>
2022-12-26 10:06:45 +0100

    Getting time elapsed for frame

--- chip8.c
+++ chip8.c
@@ -1,12 +1,13 @@
 #include <Windows.h>
 #include<stdbool.h>
+#include <stdio.h>
 #include <d3d9.h>
 
 #define clear_struct(obj)(memset(&obj, 0, sizeof(obj)))
 
-typedef struct  {
+typedef struct {
 	boolean running;
-	LPDIRECT3D9 d3d;   
+	LPDIRECT3D9 d3d;
 	LPDIRECT3DDEVICE9 d3d_dev;
 } State;
 
@@ -46,76 +47,102 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, PSTR cmd_line, i
 			0
 		);
 
-		if (window != 0) {
-			global_state.d3d = Direct3DCreate9(DIRECT3D_VERSION);
+		LARGE_INTEGER perf_freq;
 
-			if (global_state.d3d != 0)
+		if (QueryPerformanceFrequency(&perf_freq)) {
+
+			LARGE_INTEGER perf_count;
+			if (!QueryPerformanceCounter(&perf_count))
 			{
-				D3DPRESENT_PARAMETERS d3dpp;
-				clear_struct(d3dpp);
-				d3dpp.Windowed = true;
-				d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
-				d3dpp.hDeviceWindow = window;
-				HRESULT device_created = IDirect3D9_CreateDevice(
-					global_state.d3d, 
-					D3DADAPTER_DEFAULT,
-					D3DDEVTYPE_HAL,
-					window,
-					D3DCREATE_SOFTWARE_VERTEXPROCESSING,
-					&d3dpp,
-					&global_state.d3d_dev);
+				"Failed to get perfCount";
+			}
+			global_state.d3d = Direct3DCreate9(DIRECT3D_VERSION);
+			if (window != 0) {
+				if (global_state.d3d != 0)
+				{
+					D3DPRESENT_PARAMETERS d3dpp;
+					clear_struct(d3dpp);
+					d3dpp.Windowed = true;
+					d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+					d3dpp.hDeviceWindow = window;
+					HRESULT device_created = IDirect3D9_CreateDevice(
+						global_state.d3d,
+						D3DADAPTER_DEFAULT,
+						D3DDEVTYPE_HAL,
+						window,
+						D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+						&d3dpp,
+						&global_state.d3d_dev);
 
-				if (device_created == D3D_OK){
-					ShowWindow(window, cmd_show);
-					MSG msg;
-					clear_struct(msg);
+					if (device_created == D3D_OK) {
+						ShowWindow(window, cmd_show);
+						MSG msg;
+						clear_struct(msg);
 
-					global_state.running = true;
-					while (global_state.running)
-					{
-						while (PeekMessage(&msg, window, 0, 0, PM_REMOVE)) {
-							TranslateMessage(&msg);
-							DispatchMessageA(&msg);
-						}
+						global_state.running = true;
+						while (global_state.running)
+						{
+							while (PeekMessage(&msg, window, 0, 0, PM_REMOVE)) {
+								TranslateMessage(&msg);
+								DispatchMessageA(&msg);
+							}
 
-						HRESULT cleared = IDirect3DDevice9_Clear(global_state.d3d_dev,
-							0, 
-							0,
-							D3DCLEAR_TARGET,
-							D3DCOLOR_XRGB(0, 40, 100),
-							1.0f, 
-							0
-						);
+							HRESULT cleared = IDirect3DDevice9_Clear(global_state.d3d_dev,
+								0,
+								0,
+								D3DCLEAR_TARGET,
+								D3DCOLOR_XRGB(0, 40, 100),
+								1.0f,
+								0
+							);
 
-						if (cleared != D3D_OK) {
-							OutputDebugStringA("Could not clear screen");
-						}
+							if (cleared != D3D_OK) {
+								OutputDebugStringA("Could not clear screen");
+							}
 
-						if (IDirect3DDevice9_BeginScene(global_state.d3d_dev) != D3D_OK) {
-							OutputDebugStringA("BeginScene failed");
-						}
+							if (IDirect3DDevice9_BeginScene(global_state.d3d_dev) != D3D_OK) {
+								OutputDebugStringA("BeginScene failed");
+							}
 
-						if (IDirect3DDevice9_EndScene(global_state.d3d_dev) != D3D_OK) {
-							OutputDebugStringA("EndScene failed");
-						}
+							if (IDirect3DDevice9_EndScene(global_state.d3d_dev) != D3D_OK) {
+								OutputDebugStringA("EndScene failed");
+							}
 
-						if (IDirect3DDevice9_Present(global_state.d3d_dev, 0, 0, 0, 0) != D3D_OK) {
-							OutputDebugStringA("Present failed");
+							if (IDirect3DDevice9_Present(global_state.d3d_dev, 0, 0, 0, 0) != D3D_OK) {
+								OutputDebugStringA("Present failed");
+							}
+
+							LARGE_INTEGER old_perf_count = perf_count;
+							if (!QueryPerformanceCounter(&perf_count))
+							{
+								"Failed to get perfCount";
+							}
+
+							float secs_elapsed = ((float)(perf_count.QuadPart - old_perf_count.QuadPart)) / ((float)(perf_freq.QuadPart));
+
+							float milli_elapsed = secs_elapsed * 1000.0f;
+
+							char str[255];
+							sprintf(str, "Milliseconds: %f\n", milli_elapsed);
+							OutputDebugStringA(str);
 						}
 					}
+					else {
+						OutputDebugStringA("Could not create d3d device");
+					}
+
 				}
 				else {
-					OutputDebugStringA("Could not create d3d device");
+					OutputDebugStringA("Could not initialize d3d");
 				}
 
 			}
 			else {
-				OutputDebugStringA("Could not initialize d3d");
+				OutputDebugStringA("Could not open window");
 			}
-
 		}
 		else {
-			OutputDebugStringA("Could not open window");
+			OutputDebugStringA("Could not get performance frequency");
 		}
 
 	}
