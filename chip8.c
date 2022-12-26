4dc8d419e4856ae190af186b8e1a029bed2052ba
Nicola Carraro <carraron985@gmail.com>
2022-12-26 09:43:46 +0100

    Clearing screen

--- chip8.c
+++ chip8.c
@@ -47,17 +47,70 @@ int WINAPI WinMain(HINSTANCE instance, HINSTANCE prev_instance, PSTR cmd_line, i
 		);
 
 		if (window != 0) {
-			ShowWindow(window, cmd_show);
-			MSG msg;
-			clear_struct(msg);
+			global_state.d3d = Direct3DCreate9(DIRECT3D_VERSION);
 
-			global_state.running = true;
-			while (global_state.running)
+			if (global_state.d3d != 0)
 			{
-				while (PeekMessage(&msg, window, 0, 0, PM_REMOVE)) {
-					TranslateMessage(&msg);
-					DispatchMessageA(&msg);
+				D3DPRESENT_PARAMETERS d3dpp;
+				clear_struct(d3dpp);
+				d3dpp.Windowed = true;
+				d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
+				d3dpp.hDeviceWindow = window;
+				HRESULT device_created = IDirect3D9_CreateDevice(
+					global_state.d3d, 
+					D3DADAPTER_DEFAULT,
+					D3DDEVTYPE_HAL,
+					window,
+					D3DCREATE_SOFTWARE_VERTEXPROCESSING,
+					&d3dpp,
+					&global_state.d3d_dev);
+
+				if (device_created == D3D_OK){
+					ShowWindow(window, cmd_show);
+					MSG msg;
+					clear_struct(msg);
+
+					global_state.running = true;
+					while (global_state.running)
+					{
+						while (PeekMessage(&msg, window, 0, 0, PM_REMOVE)) {
+							TranslateMessage(&msg);
+							DispatchMessageA(&msg);
+						}
+
+						HRESULT cleared = IDirect3DDevice9_Clear(global_state.d3d_dev,
+							0, 
+							0,
+							D3DCLEAR_TARGET,
+							D3DCOLOR_XRGB(0, 40, 100),
+							1.0f, 
+							0
+						);
+
+						if (cleared != D3D_OK) {
+							OutputDebugStringA("Could not clear screen");
+						}
+
+						if (IDirect3DDevice9_BeginScene(global_state.d3d_dev) != D3D_OK) {
+							OutputDebugStringA("BeginScene failed");
+						}
+
+						if (IDirect3DDevice9_EndScene(global_state.d3d_dev) != D3D_OK) {
+							OutputDebugStringA("EndScene failed");
+						}
+
+						if (IDirect3DDevice9_Present(global_state.d3d_dev, 0, 0, 0, 0) != D3D_OK) {
+							OutputDebugStringA("Present failed");
+						}
+					}
 				}
+				else {
+					OutputDebugStringA("Could not create d3d device");
+				}
+
+			}
+			else {
+				OutputDebugStringA("Could not initialize d3d");
 			}
 
 		}
