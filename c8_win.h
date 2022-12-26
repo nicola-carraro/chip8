d37cbca1b2562dc84ca6ea8c27c96c4f2e941055
Nicola Carraro <carraron985@gmail.com>
2022-12-26 22:17:13 +0100

    Separated platform layer

---
+++ c8_win.h
@@ -0,0 +1,26 @@
+#ifndef C8_WIN
+#define C8_WIN
+
+#include <Windows.h>
+#include <d3d9.h>
+#include "c8_app.h"
+
+#define C8_WIN_D3D_FVF (D3DFVF_XYZRHW | D3DFVF_DIFFUSE)
+
+typedef struct {
+	FLOAT x;
+	FLOAT y;
+	FLOAT z;
+	FLOAT rhw;
+	D3DCOLOR color;
+} C8_Win_D3d_Vertex;
+
+typedef struct {
+	LPDIRECT3D9 d3d;
+	LPDIRECT3DDEVICE9 d3d_dev;
+	C8_App_State app_state;
+} C8_Win_State;
+
+C8_Win_State global_state;
+
+#endif 
