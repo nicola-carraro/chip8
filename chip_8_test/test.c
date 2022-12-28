51485d37fc73b5f81bda6eed256293cf259ff413
Nicola Carraro <carraron985@gmail.com>
2022-12-28 22:18:42 +0100

    Added a test

--- chip_8_test/test.c
+++ chip_8_test/test.c
@@ -2,17 +2,26 @@
 
 #include "../c8_win.c"
 
-void c8_test_arena_alloc()
+void c8_test_arena_init()
 {
+	printf("TEST : initialise arena\n");
+
 	C8_Arena arena;
 
-	c8_arena_init(&arena, 100, 4);
+	i32 alignement = 4;
 
-	printf("c8 test arena alloc \n");
+	psz size = 100;
 
+	c8_arena_init(&arena, size, alignement);
+
+	assert(arena.alignement == alignement);
+
+	assert(arena.max_bytes == size);
+
+	assert(arena.offset == 0);
 }
 
 int main(char** args, int argv)
 {
-	c8_test_arena_alloc();
+	c8_test_arena_init();
 }
