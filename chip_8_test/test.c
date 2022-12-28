89e955ea97869e444c9b3e50cc3904b9b513f48e
Nicola Carraro <carraron985@gmail.com>
2022-12-28 22:27:15 +0100

    Written a bunch of tests

--- chip_8_test/test.c
+++ chip_8_test/test.c
@@ -4,7 +4,7 @@
 
 void c8_test_arena_init()
 {
-	printf("TEST : initialise arena\n");
+	printf("TEST : initialize arena\n");
 
 	C8_Arena arena;
 
@@ -21,7 +21,36 @@ void c8_test_arena_init()
 	assert(arena.offset == 0);
 }
 
+void c8_test_arena_alloc_free()
+{
+	printf("TEST : allocate and free with arena\n");
+
+	C8_Arena arena1;
+
+	i32 alignement = 4;
+
+	psz size = 1024;
+
+	c8_arena_init(&arena1, size, alignement);
+
+	void* mem1 = c8_arena_alloc(&arena1, 101);
+
+	assert(arena1.offset == 104);
+
+	void* mem2 = c8_arena_alloc(&arena1, 4);
+
+	assert(arena1.offset == 108);
+
+	assert(((char*)mem2 - (char*)mem1) == 104);
+
+	c8_arena_free_all(&arena1);
+
+	assert(arena1.offset == 0);
+}
+
 int main(char** args, int argv)
 {
 	c8_test_arena_init();
+
+	c8_test_arena_alloc_free();
 }
