c32198247a8f44d12da28f13906f213aecd6c752
Nicola Carraro <carraron985@gmail.com>
2023-01-11 22:13:18 +0100

    Corrected some debug prints

--- c8_app.c
+++ c8_app.c
@@ -222,7 +222,7 @@ bool c8_app_update(C8_App_State* state) {
 	}
 
 	if (!state->program_loaded) {
-		char f_name[] = "data\\merlin";
+		char f_name[] = "data\\blinky.ch8";
 		C8_File file = c8_plat_read_file(f_name, c8_arr_count(f_name) - 1, &state->arena);
 
 		if (file.data != 0) {
@@ -484,7 +484,7 @@ bool c8_app_update(C8_App_State* state) {
 			state->var_registers[C8_FLAG_REG] = bit;
 		}
 		else if ((instruction & 0xf0ff) == 0xE09E) {
-			c8_plat_debug_out("Skip if key is pressed\n");
+			c8_plat_debug_printf("Skip if %x key is pressed\n", x);
 			u8 key = state->var_registers[x];
 
 			if (state->keypad.keys[key].ended_down) {
@@ -493,10 +493,13 @@ bool c8_app_update(C8_App_State* state) {
 
 		}
 		else if ((instruction & 0xf0ff) == 0xE0A1) {
-			c8_plat_debug_out("Skip if key is not pressed\n");
+		    c8_plat_debug_printf("Skip if %x key is not pressed\n", state->var_registers[x]);
 			u8 key = state->var_registers[x];
 			if (!state->keypad.keys[key].ended_down) {
 				state->pc += 2;
+			}
+			else {
+			int i = 0;
 			}
 
 		}
