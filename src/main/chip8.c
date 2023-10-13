09ce034fc13d55dc395079e4a1bf041e4e0abb9b
Nicola Carraro <carraron985@gmail.com>
2023-10-13 23:14:47 +0200

    Preselect rom files in file dialog

--- src/main/chip8.c
+++ src/main/chip8.c
@@ -1109,10 +1109,13 @@ void c8_load_from_file_dialog(C8_State *state)
 
 	char path[1024] = {0};
 
+	char filter[] = {"Chip 8 rom\0*.ch8"};
+
 	OPENFILENAME file_name = {
 		.lStructSize = sizeof(file_name),
 		.hwndOwner = state->window,
 		.hInstance = state->instance,
+		.lpstrFilter = filter,
 		.lpstrFile = path,
 		.nMaxFile = C8_ARRCOUNT(path),
 		.lpstrInitialDir = "data"
