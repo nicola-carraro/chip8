be1b0314becfe3dbc1a1b7e39809e237b70a5be7
Nicola Carraro <carraron985@gmail.com>
2023-10-13 23:31:00 +0200

    Deleted print statements from assets.c

--- build.bat
+++ build.bat
@@ -2,20 +2,16 @@
 
 pushd build
 
-REM taskkill /IM remedybg.exe /F
-
 cl ../src/main/chip8.c /WX  /W4 /Z7  -nologo -Od  kernel32.lib user32.lib D3d9.lib Dsound.lib  ole32.lib Comdlg32.lib 
 
 cl ../src/test/test.c   /WX /W4 /Z7 -nologo -Od  kernel32.lib user32.lib D3d9.lib Dsound.lib /link ole32.lib Comdlg32.lib  /SUBSYSTEM:CONSOLE
 
  cl ../src/assets/assets.c  /WX /W4 /Z7 -Od -nologo -Od  kernel32.lib user32.lib D3d9.lib  Comdlg32.lib Dsound.lib 
 
-REM c8_assets.exe
+assets.exe
 
-REM c8_test.exe
+test.exe
 
 del *.ilk *.obj
-
-REM start remedybg c8_win.exe
 
 popd
